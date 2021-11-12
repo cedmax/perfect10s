978a08171ef0b2bf8572e1aa6cb5030f0af5a3ca
Marco Cedaro <cedmax@users.noreply.github.com>
2021-11-12 00:53:27 +0000

    fancier UI

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import Album from './Album'
 import data from '../new-data.json'
 
@@ -7,10 +7,28 @@ const sorted = data.sort(({ year: a = 0 }, { year: b = 0 }) => {
 })
 
 function App ({ action, playing }) {
+  const [background, setBackground] = useState()
+  const onPlay = useCallback(({ img, url }) => {
+    setBackground(img)
+    
+    //action(url)
+  }, [action])
+
+  console.log(background)
+
   return (
-    <ul>
+    <ul
+      style={{
+        '--background': `url('${background}')`
+      }}
+    >
       {sorted.map(album => (
-        <Album action={action} isPlaying={playing===album.spotify} key={album.slug} album={album} />
+        <Album
+          action={onPlay}
+          isPlaying={playing === album.spotify}
+          key={album.slug}
+          album={album}
+        />
       ))}
     </ul>
   )
